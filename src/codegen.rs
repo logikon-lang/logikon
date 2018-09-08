20e00c955b05f06b92962b632e0e9bb64d78f6c4
Alex Beregszaszi <alex@rtfs.hu>
2018-09-09 00:11:37 +0100

    Translate Function.cases instread of just functions

--- src/codegen.rs
+++ src/codegen.rs
@@ -4,16 +4,37 @@ use self::yultsur::yul::*;
 
 use ast;
 
-fn compile_function(function: &ast::Function) -> Statement {
-    let name:Identifier = Identifier { identifier: function.name.clone() };
-    let block:Block = Block { statements: vec![] };
+fn compile_case(name: &str, case: &ast::Case) -> Statement {
+    let name:Identifier = Identifier { identifier: name.to_string() };
+    let mut parameters:Vec<Identifier> = vec![];
+    let mut returns:Vec<Identifier> = vec![];
+    let mut block:Block = Block { statements: vec![] };
+
+    for parameter in &case.parameters {
+        parameters.push(Identifier { identifier: parameter.name.clone() });
+    }
+
+    returns.push(Identifier { identifier: case.return_value.name.clone() });
+
+    // FIXME: translate case.expressions -> block.statements
 
     Statement::FunctionDefinition(
-        name.clone(),
-        vec![],
-        vec![],
+        name,
+        parameters,
+        returns,
         block
     )
+}
+
+fn compile_function(function: &ast::Function) -> Statement {
+    let mut statements = vec![];
+
+    for (i, case) in function.cases.iter().enumerate() {
+        let name = format!("{}_{}", function.name, i);
+        statements.push(compile_case(&name, case));
+    }
+
+    Statement::Block(Block { statements: statements })
 }
 
 pub fn logikon_compile(contract: &ast::Contract) -> String {
@@ -48,7 +69,7 @@ mod tests {
 
         assert_eq!(
             logikon_compile(&ast::logikon_parse(&source)),
-            "{ }"
+            "{ { function f_0(a) -> (x) { } } }"
         );
     }
 }
