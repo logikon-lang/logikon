b2ced27be57c93c781b9d9d3b77dc7ca4be67f61
schaeff <thibaut@schaeff.fr>
2018-09-08 11:50:48 +0200

    move example to tests, get source from file

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,50 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
-
 #[derive(Parser)]
 #[grammar = "logikon.pest"]
 struct IdentParser;
 
-fn main() {
-    let pairs = IdentParser::parse(Rule::contract, "declare balance public Array. declare lala public UInt. define f public (). define g public ({a} b) x :- (f (f (and a b))).").unwrap_or_else(|e| panic!("{}", e));
+fn main() {}
 
-    // Because ident_list is silent, the iterator will contain idents
-    for pair in pairs {
+#[cfg(test)]
+mod tests {
 
-        let span = pair.clone().into_span();
-        // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", span);
-        println!("Text:    {}", span.as_str());
+    use super::*;
+    use pest::Parser;
+    use std::fs::File;
+    use std::io::prelude::*;
 
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            let inner_span = inner_pair.clone().into_span();
-            match inner_pair.as_rule() {
-                Rule::contract => println!("Contract:  {}", inner_span.as_str()),
-                Rule::state_var_decl => println!("StateVarDecl:   {}", inner_span.as_str()),
-                Rule::logikon_type => println!("Type:   {}", inner_span.as_str()),
-                Rule::visibility => println!("Visibility:   {}", inner_span.as_str()),
-                Rule::identifier => println!("Identifier:   {}", inner_span.as_str()),
-                Rule::function_def => println!("FunctionDefinition:   {}", inner_span.as_str()),
-                _ => unreachable!()
-            };
+    fn file_to_string(path: &str) -> String {
+        let mut file = File::open(path).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+        content
+    }
+
+    #[test]
+    fn basic_syntax() {
+        let source = file_to_string("./examples/syntax.lk");
+
+        let pairs = IdentParser::parse(Rule::contract, &source).unwrap();
+
+        // Because ident_list is silent, the iterator will contain idents
+        for pair in pairs {
+            let span = pair.clone().into_span();
+            // A pair is a combination of the rule which matched and a span of input
+            println!("Rule:    {:?}", pair.as_rule());
+            println!("Span:    {:?}", span);
+            println!("Text:    {}", span.as_str());
+
+            // A pair can be converted to an iterator of the tokens which make it up:
+            for inner_pair in pair.into_inner() {
+                let inner_span = inner_pair.clone().into_span();
+                match inner_pair.as_rule() {
+                    Rule::state_var_decl => println!("StateVarDecl:   {}", inner_span.as_str()),
+                    Rule::function_def => println!("FunctionDefinition:   {}", inner_span.as_str()),
+                    _ => unreachable!(),
+                };
+            }
         }
     }
 }
