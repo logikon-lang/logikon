ae2d106e950fb7c6b1fe8cb85594dfac8ef09362
Alex Beregszaszi <alex@rtfs.hu>
2018-09-08 23:23:54 +0100

    Add first try at code generating a simple function

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,12 +1,29 @@
 extern crate yultsur;
 
-use self::yultsur::*;
+use self::yultsur::yul::*;
 
 use ast;
 
+fn compile_function(function: &ast::Function) -> Statement {
+    let name:Identifier = Identifier { identifier: function.name.clone() };
+    let block:Block = Block { statements: vec![] };
+
+    Statement::FunctionDefinition(
+        name.clone(),
+        vec![],
+        vec![],
+        block
+    )
+}
+
 pub fn logikon_compile(contract: &ast::Contract) -> String {
     let mut statements = vec![];
-    yul::Block { statements: statements }.to_string()
+
+    for function in &contract.functions {
+        statements.push(compile_function(&function))
+    }
+
+    Block { statements: statements }.to_string()
 }
 
 #[cfg(test)]
@@ -16,6 +33,18 @@ mod tests {
     #[test]
     fn smoke() {
         let source = "";
+
+        assert_eq!(
+            logikon_compile(&ast::logikon_parse(&source)),
+            "{ }"
+        );
+    }
+
+    #[test]
+    fn identity_fn() {
+        let source = r#"define f (Bool) -> Bool
+        case (a) x :-
+            (= x a)."#;
 
         assert_eq!(
             logikon_compile(&ast::logikon_parse(&source)),
