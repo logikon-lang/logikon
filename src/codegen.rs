d4b52313cb682505a09d4a68152f17a3406dc6b9
Alex Beregszaszi <alex@rtfs.hu>
2018-09-09 09:56:41 +0100

    Panic on unsupported expression

--- src/codegen.rs
+++ src/codegen.rs
@@ -30,10 +30,7 @@ fn compile_expression(exp: &ast::BooleanExpression) -> Statement {
             // Statement::Expression(Expression::FunctionCall(Identifier::new("eq"), vec![left, right]))
             Statement::Assignment(vec![get_identifier(left)], right)
         },
-        c => {
-            println!("{:?}", c);
-            Statement::Expression(Expression::Literal(Literal { literal: "1".to_string() }))
-        },
+        c => panic!("Unsupported expression."),
     }
 }
 
